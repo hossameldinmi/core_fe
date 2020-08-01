bd9e97759a3d8db67b154c5ba50fb8c6f49f709c
hossameldinmi <hossameldin2022@gmail.com>
2020-08-01 23:53:54 +0200

    date_time_format equality tests

---
+++ core_fe_flutter/test/enums/date_time_format_test.dart
@@ -0,0 +1,11 @@
+import 'package:core_fe_flutter/enums.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('equality', () {
+    expect(DateTimeFormat.isoFormat, DateTimeFormat.isoFormat);
+    expect(DateTimeFormat.shortDateFormat, DateTimeFormat.shortDateFormat);
+    expect(DateTimeFormat.isoFormat,
+        isNot(equals(DateTimeFormat.shortDateFormat)));
+  });
+}
