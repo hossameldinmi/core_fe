404eb3a0b92bd8cbdb2807c8e72925000336d3a8
Hossam Eldin Mahmoud <hossameldinmi@gmail.com>
2021-02-10 04:11:12 +0200

    delegates_extensions, timeInDay toString update

--- core_fe_dart/lib/extensions.dart
+++ core_fe_dart/lib/extensions.dart
@@ -7,3 +7,4 @@ export 'package:core_fe_dart/src/extensions/future_extensions.dart';
 export 'package:core_fe_dart/src/extensions/stream_extensions.dart';
 export 'package:core_fe_dart/src/extensions/duration_extensions.dart';
 export 'package:core_fe_dart/src/extensions/date_time_extensions.dart';
+export 'package:core_fe_dart/src/extensions/delegates_extensions.dart';
